c2a9b2d0a5a26c4ea1fbba50e7d7cd3ecf804edb
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-09-19 13:10:24 -0400

    says the temperature of the microbit

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack
+ * Created on: Sep 2025
+ * This program says the temperature
 */
 
-basic.showString('Hello, World!')
+//variable for the temperature of the microbit
+let temperatureOfMicrobit: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+   input.temperature()
+    basic.showNumber(input.temperature())
+})
